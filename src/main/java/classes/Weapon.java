e686a6c40860775c44429c480cb86809e541d6ac
Sergio <estrada.serg@gmail.com>
2024-10-14 09:31:16 -0500

    created weapon class

---
+++ src/main/java/classes/Weapon.java
@@ -0,0 +1,22 @@
+package classes;
+
+import abstracted.Item;
+
+public class Weapon extends Item {
+
+    public Weapon(String fileName, boolean locked) {
+        super(fileName, locked);
+    }
+
+    public void adjustCondition(){
+
+    }
+
+    public void adjustModifier(){
+
+    }
+
+    public void adjustValue(){
+
+    }
+}
